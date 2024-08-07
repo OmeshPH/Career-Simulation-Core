921954e49a8fb6ff090ae05265c889e1406b21aa
OmeshPH <OmeshPH@gmail.com>
2024-08-07 12:08:35 -0400

    modifying layout

--- review-site/src/App.jsx
+++ review-site/src/App.jsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React from 'react';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    
-    </>
-  )
+    <div>
+      <h1>Review Site</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
